4d12ed1818b1d5b44bde160d35b0854c0a5e0de4
NanaHemaa <judithnanahemaab@gmail.com>
2023-06-21 04:22:12 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,20 +5,18 @@
  * Return: void
  */
 
-void print_alphabet(void)
+void print_alphabet_x10(void)
 {
-char ch;
-int i;
-i = 0;
-while (i < 10)
-{
-for (ch = 'a'; ch <= 'z'; ch++)
-{
-_putchar(ch);
-}
-_putchar('\n');
-i++;
-}
+	char c;
+	int i = 0;
 
-
+	while (i < 10)
+	{
+	for (c = 'a'; c <= 'z'; c++)
+	{
+	_putchar(c);
+	}
+	_putchar('\n');
+	i++;
+	}
 }

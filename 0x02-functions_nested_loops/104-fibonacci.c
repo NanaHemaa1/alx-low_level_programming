7ba1fea387e7599714294cffb5ab56eab5c9a7a2
NanaHemaa <judithnanahemaab@gmail.com>
2023-06-21 10:26:00 -0700

    104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,70 +1,52 @@
-#include "main.h"
 #include <stdio.h>
 
-/**
- * numl_ength - returns the length of string
- *
- * @num: oprand number
- *
- * Return: number of digits
- */
-
-int numl_ength(int num)
-{
-	int length = 0;
-
-	if (!num)
-		return (1);
-
-	while (num)
-	{
-		num = num / 10;
-		length += 1;
-	}
-
-	return (length);
-}
 
 /**
- * main - Entry point
+ * main - fibonacci <3
  *
- * Description: prints the first 98 fibonacci numbers
- *      starting with 1 and 2 followed by a new line
+ * Purpose - no hardcode
  *
- * Return: Always 0 (success)
+ * Return:  (Success)
  */
+
 
 int main(void)
 {
-	int count, initial0s;
-	unsigned long f1 = 1, f2 = 2, sum, mx = 100000000, f1o = 0, f2o = 0, sumo = 0;
+	unsigned long int i;
+	unsigned long int bef = 1;
+	unsigned long int aft = 2;
+	unsigned long int l = 1000000000;
+	unsigned long int bef1;
+	unsigned long int bef2;
+	unsigned long int aft1;
+	unsigned long int aft2;
 
-	for (count = 1; count <= 98; count++)
+
+	printf("%lu", bef);
+
+
+	for (i = 1; i < 91; i++)
 	{
-		if (f1o > 0)
-			printf("%lu", f1o);
-		initial0s = numl_ength(mx) - 1 - numl_ength(f1);
-
-		while (f1o > 0 && initial0s > 0)
-		{
-			printf("%d", 0);
-			initial0s--;
-		}
-
-		printf("%lu", f1);
-
-		sum = (f1 + f2) % mx;
-		sum = f1o + f2o + (f1 + f2) / mx;
-		f1 = f2;
-		f1o = f2o;
-		f2 = sum;
-		f2o = sumo;
-
-		if (count != 98)
-			printf(",");
-		else
-			printf("\n");
+		printf(", %lu", aft);
+		aft += bef;
+		bef = aft - bef;
 	}
 
+
+	bef1 = (bef / l);
+	bef2 = (bef % l);
+	aft1 = (aft / l);
+	aft2 = (aft % l);
+
+	for (i = 92; i < 99; ++i)
+	{
+		printf(", %lu", aft1 + (aft2 / l));
+		printf("%lu", aft2 % l);
+		aft1 = aft1 + bef1;
+		bef1 = aft1 - bef1;
+		aft2 = aft2 + bef2;
+		bef2 = aft2 - bef2;
+	}
+	printf("\n");
 	return (0);
 }

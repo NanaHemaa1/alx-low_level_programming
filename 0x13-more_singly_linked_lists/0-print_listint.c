9aff1ff2bea1f423f3bc5f9b563527feca6ca380
NanaHemaa <judithnanahemaab@gmail.com>
2023-08-01 04:33:46 -0700

    0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,14 +8,14 @@
  */
 size_t print_listint(const listint_t *h)
 {
-	size_t num = 0;
+	size_t act = 0;
 
 	while (h)
 	{
 		printf("%d\n", h->n);
-		num++;
+		act++;
 		h = h->next;
 	}
 
-	return (num);
+	return (act);
 }
